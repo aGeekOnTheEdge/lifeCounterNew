692ff6bc80d3627f8f20f1983ffb03827d02345e
carlos@ageekontheedge.com <carl.heasman@bristowgroup.com>
2019-11-07 13:47:25 +0000

    simplified functions to one

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,13 @@
 export class App {
   public heading: string;
-  public lifeTotal: number = 20;
+  lifeTotal = 20;
 
   constructor() {
     this.heading = 'MTG Life Counter';
-
   }
 
-    addOne() {
-      var lifeTotal = document.getElementById('currentLife').innerHTML;
-      lifeTotal = Number(lifeTotal) + 1;
-      document.getElementById('currentLife').innerHTML = lifeTotal;
-    }
-
-    addFive() {
-      var lifeTotal = document.getElementById('currentLife').innerHTML;
-      lifeTotal = Number(lifeTotal) + 5;
-      document.getElementById('currentLife').innerHTML = lifeTotal;
-    }
-
-    subOne() {
-      var lifeTotal = document.getElementById('currentLife').innerHTML;
-      lifeTotal = Number(lifeTotal) - 1;
-      document.getElementById('currentLife').innerHTML = lifeTotal;
-    }
-
-    subFive() {
-      var lifeTotal = document.getElementById('currentLife').innerHTML;
-      lifeTotal = Number(lifeTotal) - 5;
-      document.getElementById('currentLife').innerHTML = lifeTotal;
-    }
+  adjustLife(modifier: number) {
+    this.lifeTotal = this.lifeTotal + modifier
+  }
 
 }
